680a69e270443ba7e8fb16b844853bbfed2f0a47
jenifers-s <67614296+jeniferss@users.noreply.github.com>
2022-08-08 14:37:19 -0300

    update logs

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -69,9 +69,10 @@ function introspect(oidcConfig)
   if utils.is_ms_token() == true and oidcConfig.verify_ms_token == false then
     if oidcConfig.ms_token_public_key == nil then utils.exit(ngx.HTTP_UNAUTHORIZED, 'Missing Public Key', ngx.HTTP_UNAUTHORIZED) end
     if utils.verify_signature(oidcConfig.ms_token_public_key) == false then utils.exit(ngx.HTTP_UNAUTHORIZED, 'Invalid Signature', ngx.HTTP_UNAUTHORIZED) end
-    return nil
+    ngx.log(ngx.ALERT, '### SKIPPING REQUEST ###')
+    return utils.claims
   end
-
+  ngx.log(ngx.ALERT, '### MAKING REQUEST ###')
   if utils.has_bearer_access_token() or oidcConfig.bearer_only == "yes" then
     local res, err = require("resty.openidc").introspect(oidcConfig)
     if err then
